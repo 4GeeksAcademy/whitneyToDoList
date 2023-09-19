7bbc056c83b4dc30bfc4c7334f5d0b8de1bff0fc
Swtpea1516 <134784015+Swtpea1516@users.noreply.github.com>
2023-09-19 18:43:44 +0000

    troubleshoot

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,71 @@
-import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useEffect, useState } from "react";
 
-//create your first component
+
+
+
+
 const Home = () => {
+	const [inputValue, setInputValue] = useState("")
+	const [List, setList] = useState([])
+
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+
+		<div className="container">
+			<h1> My Todos</h1>
+			<ul>
+				<li>
+					<div className="d-flex">
+						<input
+							type="text"
+							value={inputValue}
+							onChange={(e) => setInputValue(e.target.value)}>what do you need to do</input>
+						<button onClick={() => {
+							setList(List.concat([inputValue]));
+							setInputValue("");
+						}}>Click</button>
+
+					</div>
+				</li>
+
+
+				{
+					List.map((task) => (<li>{task}</li>))
+				}
+
+
+
+
+			</ul>
+			<div>23 tasks </div>
 		</div>
-	);
-};
+
+	)
+}
+
 
 export default Home;
+
+
+
+// const Home = () => {
+// 	return (
+// 		<div className="text-center">
+// 			<h1 className="text-center mt-5">Hello Rigo!</h1>
+// 			<p>
+// 				<img src={rigoImage} />
+// 			</p>
+// 			<a href="#" className="btn btn-success">
+// 				If you see this green button... bootstrap is working...
+// 			</a>
+// 			<p>
+// 				Made by{" "}
+// 				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
+// 				love!
+// 			</p>
+// 		</div>
+	// );i class="fa-regular fa-trash-can"></i>
+// };
+
+
